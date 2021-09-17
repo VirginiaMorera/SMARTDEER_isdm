a632180c84884aaaf1eb9e254059e974cca61035
VirginiaMorera <morera.virginia@gmail.com>
2021-09-17 17:51:23 +0100

    add scripts from Kevin Morelle) for covar extraction

--- scripts/setups.R
+++ scripts/setups.R
@@ -8,4 +8,4 @@ library(INLA)
 
 '%!in%' <- function(x,y)!('%in%'(x,y))
 
-ITM <- CRS("+proj=tmerc +lat_0=53.5 +lon_0=-8 +k=0.99982 +x_0=600000 +y_0=750000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=km +no_defs")
+ITM_km <- CRS("+proj=tmerc +lat_0=53.5 +lon_0=-8 +k=0.99982 +x_0=600000 +y_0=750000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=km +no_defs")
