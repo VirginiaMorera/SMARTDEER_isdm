d089c77789d6b985d51252850c7f368055496e49
VirginiaMorera <morera.virginia@gmail.com>
2021-11-01 17:38:19 +0000

    addition of packages needed for new analyses

--- scripts/setups.R
+++ scripts/setups.R
@@ -7,6 +7,8 @@ library(inlabru)
 library(INLA)
 library(rasterVis)
 library(ggcorrplot)
+library(ggthemes)
+library(cowplot)
 '%!in%' <- function(x,y)!('%in%'(x,y))
 
 ITM_km <- CRS("+proj=tmerc +lat_0=53.5 +lon_0=-8 +k=0.99982 +x_0=600000 +y_0=750000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=km +no_defs")
