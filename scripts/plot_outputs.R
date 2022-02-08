3bc442d8c3c6402fabb0e21a3e73854abb7e5ec2
VirginiaMorera <morera.virginia@gmail.com>
2022-02-08 11:45:26 +0000

    Add code to plot output of all species

--- scripts/plot_outputs.R
+++ scripts/plot_outputs.R
@@ -1,38 +1,242 @@
-# load data
+# load ireland data
 ireland <- st_read("data/ireland_ITM.shp")
 
-spde <- readRDS("cluster_outputs/spde_range100_sd2.RDS")
-ireland_proj <- st_transform(ireland, sp_pred@proj4string)
+
+## Red deer ##
+
+# load model
+mdl_RD <- readRDS("cluster_outputs/mdl_RD_def.RDS")
+
+fixed.effects <- mdl_RD$summary.fixed
+fixed.effects$variable <- row.names(fixed.effects)
+
+fixed.effects <- fixed.effects %>% 
+  filter(variable %!in% c("PO_data_sel_intercept", "PA_data_sel_intercept"))
+
+names(fixed.effects) <- c("mean", "sd", "lower", "median", "higher", "mode", "kld", "variable")
+
+# fixed.effects$variable <- c("Tree cover density", "Elevation", "Slope",
+#                             "Human footprint index", "Land cover")
+
+fixed.effects$variable <- c("Tree cover density", "Elevation", "Slope",
+                            "Human footprint index", "Forest distance", "Small Woody Features")
+
+
+fixedRD <- fixed.effects %>% 
+  # filter(variable != "Human footprint index") %>%
+  ggplot(aes(x = variable, 
+             # y = median,
+             y = mean,
+             colour = variable)) + 
+  # geom_errorbar(aes(ymin = lower, ymax = higher), width =.1) +
+  geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), width =.1) +
+  geom_line() +
+  geom_point() + 
+  geom_hline(yintercept = 0) + 
+  theme_bw() + 
+  coord_flip() + 
+  labs(title = "Covariate effects Red deer", y = "Median", x = "", colour = "Covariate") +
+  NULL
+
+
+# map outputs
+RD_pred_lin <- readRDS("cluster_outputs/RD_pred_lin_def.RDS")
+RD_pred_resp <- readRDS("cluster_outputs/RD_pred_resp_def.RDS")
+RD_spde_PO <- readRDS("cluster_outputs/RD_spde_PO_def.RDS")
+RD_spde_PA <- readRDS("cluster_outputs/RD_spde_PA_def.RDS")
+
+ireland_proj <- st_transform(ireland, RD_pred_lin@proj4string)
 ireland_sp <- as_Spatial(ireland_proj)
 
-sp_pred <- readRDS("cluster_outputs/sppred_range100_sd2.RDS")
+
+pred_lin <- stack(raster(RD_pred_lin['mean']), raster(RD_pred_lin['sd']))
+pred_resp <- stack(raster(RD_pred_resp['mean']), raster(RD_pred_resp['sd']))
+spde_PO <- stack(raster(RD_spde_PO['mean']), raster(RD_spde_PO['sd']))
+spde_PA <- stack(raster(RD_spde_PA['mean']), raster(RD_spde_PA['sd']))
 
 
-pred <- stack(raster(sp_pred['mean']), raster(sp_pred['sd']), raster(sp_pred['median']), 
-              raster(sp_pred['q0.025']), raster(sp_pred['q0.975']))
+# Plot 
 
-spderas <- stack(raster(spde['mean']), raster(spde['sd']), raster(spde['median']), 
-              raster(spde['q0.025']), raster(spde['q0.975']))
+plotlayer <- pred_lin
 
-spderas$IQR <- spderas$q0.975 - spderas$q0.025
-
-# (plt <- levelplot(pred$median, col.regions = viridis(16), 
-#                   xlab = "Longitude", ylab = "Latitude", 
-#                   # xlim = c(-22, -7), ylim = c(15, 33), 
-#                   margin = TRUE, zscaleLog = FALSE) +
-#     latticeExtra::layer(sp.polygons(ireland_sp, lwd = 1, col = "darkgray")) +
-#     latticeExtra::layer(sp.polygons(nha_sp, lwd = 0.1, col = "pink")))
-
-spderas2 <- subset(spderas, c(1,2,3,6))
-
-nl <- nlayers(spderas2)
-m <- matrix(1:nl, nrow=2)
+nl <- nlayers(plotlayer)
+m <- matrix(1:nl, ncol = 2)
 for (i in 1:nl){
-  p <- levelplot(spderas2, layers=i, col.regions = viridis(16), 
+  p <- levelplot(plotlayer, layers=i, col.regions = viridis(16), 
                  xlab = "Longitude", ylab = "Latitude", 
+                 zscaleLog = FALSE,
                  # xlim = c(-22, -7), ylim = c(15, 33), 
-                 margin = FALSE, zscaleLog = FALSE, main = names(spderas2)[i]) + 
+                 margin = FALSE, main = names(plotlayer)[i]) + 
     latticeExtra::layer(sp.polygons(ireland_sp, lwd = 1, col = "darkgray"))
   print(p, split=c(col(m)[i], row(m)[i], ncol(m), nrow(m)), more=(i<nl))
 }
   
+spdes <- stack(spde_PO$mean, spde_PA$mean)
+names(spdes) <- c("PO", "PA")
+
+levelplot(spdes, col.regions = viridis(16), 
+          xlab = "Longitude", ylab = "Latitude", 
+          zscaleLog = FALSE,
+          # xlim = c(-22, -7), ylim = c(15, 33), 
+          margin = FALSE, main = names(spdes)) + 
+  latticeExtra::layer(sp.polygons(ireland_sp, lwd = 1, col = "darkgray"))
+
+
+## Sika deer ##
+
+# load model
+mdl_SD <- readRDS("cluster_outputs/mdl_SD_def.RDS")
+
+fixed.effects <- mdl_SD$summary.fixed
+fixed.effects$variable <- row.names(fixed.effects)
+
+fixed.effects <- fixed.effects %>% 
+  filter(variable %!in% c("PO_data_sel_intercept", "PA_data_sel_intercept"))
+
+names(fixed.effects) <- c("mean", "sd", "lower", "median", "higher", "mode", "kld", "variable")
+
+# fixed.effects$variable <- c("Tree cover density", "Elevation", "Slope",
+#                             "Human footprint index", "Land cover")
+
+fixed.effects$variable <- c("Tree cover density", "Elevation", "Slope",
+                            "Human footprint index", "Forest distance", "Small Woody Features")
+
+
+fixedSD <- fixed.effects %>% 
+  # filter(variable != "Human footprint index") %>%
+  ggplot(aes(x = variable, 
+             # y = median,
+             y = mean,
+             colour = variable)) + 
+  # geom_errorbar(aes(ymin = lower, ymax = higher), width =.1) +
+  geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), width =.1) +
+  geom_line() +
+  geom_point() + 
+  geom_hline(yintercept = 0) + 
+  theme_bw() + 
+  coord_flip() + 
+  labs(title = "Covariate effects Sika deer", y = "Median", x = "", colour = "Covariate") +
+  NULL
+
+
+# map outputs
+SD_pred_lin <- readRDS("cluster_outputs/SD_pred_lin_def.RDS")
+SD_pred_resp <- readRDS("cluster_outputs/SD_pred_resp_def.RDS")
+SD_spde_PO <- readRDS("cluster_outputs/SD_spde_PO_def.RDS")
+SD_spde_PA <- readRDS("cluster_outputs/SD_spde_PA_def.RDS")
+
+ireland_proj <- st_transform(ireland, SD_pred_lin@proj4string)
+ireland_sp <- as_Spatial(ireland_proj)
+
+
+pred_lin <- stack(raster(SD_pred_lin['mean']), raster(SD_pred_lin['sd']))
+pred_resp <- stack(raster(SD_pred_resp['mean']), raster(SD_pred_resp['sd']))
+spde_PO <- stack(raster(SD_spde_PO['mean']), raster(SD_spde_PO['sd']))
+spde_PA <- stack(raster(SD_spde_PA['mean']), raster(SD_spde_PA['sd']))
+
+
+# Plot 
+
+plotlayer <- pred_resp
+
+nl <- nlayers(plotlayer)
+m <- matrix(1:nl, ncol = 2)
+for (i in 1:nl){
+  p <- levelplot(plotlayer, layers=i, col.regions = viridis(16), 
+                 xlab = "Longitude", ylab = "Latitude", 
+                 zscaleLog = FALSE,
+                 # xlim = c(-22, -7), ylim = c(15, 33), 
+                 margin = FALSE, main = names(plotlayer)[i]) + 
+    latticeExtra::layer(sp.polygons(ireland_sp, lwd = 1, col = "darkgray"))
+  print(p, split=c(col(m)[i], row(m)[i], ncol(m), nrow(m)), more=(i<nl))
+}
+
+spdes <- stack(spde_PO$mean, spde_PA$mean)
+names(spdes) <- c("PO", "PA")
+
+levelplot(spdes, col.regions = viridis(16), 
+          xlab = "Longitude", ylab = "Latitude", 
+          zscaleLog = FALSE,
+          # xlim = c(-22, -7), ylim = c(15, 33), 
+          margin = FALSE, main = names(spdes)) + 
+  latticeExtra::layer(sp.polygons(ireland_sp, lwd = 1, col = "darkgray"))
+
+## Fallow deer ##
+
+# load model
+mdl_FD <- readRDS("cluster_outputs/mdl_FD_def.RDS")
+
+fixed.effects <- mdl_FD$summary.fixed
+fixed.effects$variable <- row.names(fixed.effects)
+
+fixed.effects <- fixed.effects %>% 
+  filter(variable %!in% c("PO_data_sel_intercept", "PA_data_sel_intercept"))
+
+names(fixed.effects) <- c("mean", "FD", "lower", "median", "higher", "mode", "kld", "variable")
+
+# fixed.effects$variable <- c("Tree cover density", "Elevation", "Slope",
+#                             "Human footprint index", "Land cover")
+
+fixed.effects$variable <- c("Tree cover density", "Elevation", "Slope",
+                            "Human footprint index", "Forest distance", "Small Woody Features")
+
+
+fixedFD <- fixed.effects %>% 
+  # filter(variable != "Human footprint index") %>%
+  ggplot(aes(x = variable, 
+             # y = median,
+             y = mean,
+             colour = variable)) + 
+  # geom_errorbar(aes(ymin = lower, ymax = higher), width =.1) +
+  geom_errorbar(aes(ymin = mean - FD, ymax = mean + FD), width =.1) +
+  geom_line() +
+  geom_point() + 
+  geom_hline(yintercept = 0) + 
+  theme_bw() + 
+  coord_flip() + 
+  labs(title = "Covariate effects Fallow deer", y = "Median", x = "", colour = "Covariate") +
+  NULL
+
+cowplot::plot_grid(plotlist = list(fixedRD, fixedSD, fixedFD), ncol = 2)
+
+# map outputs
+FD_pred_lin <- readRDS("cluster_outputs/FD_pred_lin_def.RDS")
+FD_pred_resp <- readRDS("cluster_outputs/FD_pred_resp_def.RDS")
+FD_spde_PO <- readRDS("cluster_outputs/FD_spde_PO_def.RDS")
+FD_spde_PA <- readRDS("cluster_outputs/FD_spde_PA_def.RDS")
+
+ireland_proj <- st_transform(ireland, FD_pred_lin@proj4string)
+ireland_sp <- as_Spatial(ireland_proj)
+
+
+pred_lin <- stack(raster(FD_pred_lin['mean']), raster(FD_pred_lin['sd']))
+pred_resp <- stack(raster(FD_pred_resp['mean']), raster(FD_pred_resp['sd']))
+spde_PO <- stack(raster(FD_spde_PO['mean']), raster(FD_spde_PO['sd']))
+spde_PA <- stack(raster(FD_spde_PA['mean']), raster(FD_spde_PA['sd']))
+
+
+# Plot 
+
+plotlayer <- pred_resp
+
+nl <- nlayers(plotlayer)
+m <- matrix(1:nl, ncol = 2)
+for (i in 1:nl){
+  p <- levelplot(plotlayer, layers=i, col.regions = viridis(16), 
+                 xlab = "Longitude", ylab = "Latitude", 
+                 zscaleLog = FALSE,
+                 # xlim = c(-22, -7), ylim = c(15, 33), 
+                 margin = FALSE, main = names(plotlayer)[i]) + 
+    latticeExtra::layer(sp.polygons(ireland_sp, lwd = 1, col = "darkgray"))
+  print(p, split=c(col(m)[i], row(m)[i], ncol(m), nrow(m)), more=(i<nl))
+}
+
+spdes <- stack(spde_PO$mean, spde_PA$mean)
+names(spdes) <- c("PO", "PA")
+
+levelplot(spdes, col.regions = viridis(16), 
+          xlab = "Longitude", ylab = "Latitude", 
+          zscaleLog = FALSE,
+          # xlim = c(-22, -7), ylim = c(15, 33), 
+          margin = FALSE, main = names(spdes)) + 
+  latticeExtra::layer(sp.polygons(ireland_sp, lwd = 1, col = "darkgray"))
