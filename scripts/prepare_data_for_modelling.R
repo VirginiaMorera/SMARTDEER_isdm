1405c9cfbcaab1913554bd1253a1d5168f3dcabd
VirginiaMorera <morera.virginia@gmail.com>
2022-02-25 14:16:57 +0000

    silly format changes

--- scripts/prepare_data_for_modelling.R
+++ scripts/prepare_data_for_modelling.R
@@ -10,18 +10,19 @@ ireland_km <- ireland %>%
   st_transform(projKM)
 
 sel_data <- all_data %>% 
-  dplyr::select(County, Year, Species, Deer.Presence, Deer.Count, Y = Latitude, X = Longitude, Source, Type) %>% 
+  dplyr::select(County, Site_id, Year, Species, Deer.Presence, Deer.Count, 
+                Y = Latitude, X = Longitude, Source, Type) %>% 
   filter(X > 421874) %>% 
   st_as_sf(coords = c("X", "Y")) %>% 
   st_set_crs(st_crs(ireland)) %>% 
   st_transform(projKM) # IRENET in km
 
 sel_data %>% 
-  filter(Type == "PO") %>%
+  filter(Type == "PA") %>%
   filter(Species == "FallowDeer") %>% 
   filter(Source != "CameraTraps") %>%
   ggplot + 
-  geom_sf(aes(col = Source), alpha = 0.5) + 
+  geom_sf(aes(col = Deer.Presence), alpha = 0.5) + 
   geom_sf(data = ireland_km, fill = NA, col = "darkgray") + 
   # facet_wrap(~Type) + 
   theme_bw()
@@ -33,7 +34,12 @@ PA_data <- sel_data %>%
                 Deer.Presence = if_else(Deer.Presence == "Yes", 1, 0)) %>% 
   rename(PA = Deer.Presence) %>% 
   dplyr::select(-Deer.Count) %>% 
-  st_set_geometry(NULL)
+  st_set_geometry(NULL) %>% 
+  group_by(Species, Site_id, Source, Type, X, Y) %>% 
+  summarise(count_any = sum(PA)) %>% 
+  ungroup() %>% 
+  mutate(Deer.Presence = if_else(count_any == 0, 0, 1)) %>% 
+  dplyr::select(-count_any)
 
 write.csv(PA_data, file = "data/PA_data_all.csv", row.names = F)
 
@@ -65,14 +71,23 @@ PA_covars <- raster::extract(env_data, PA_data_sf)
 
 PA_covars <- as.data.frame(cbind(PA = PA_data$PA, PA_covars)) 
 PA_covars <- PA_covars %>% 
-  mutate(landCover2 = recode(landCover, "1" = "Built area", "2" = "Saltwater related", "3" = "No vegetation", 
+  mutate(landCover2 = recode(landCover, "1" = "Built area", 
+                             "2" = "Saltwater related", "3" = "No vegetation", 
                              "4" = "Freshwater related", "5" = "Other vegetation", "6" = "Agricultural land", 
-                             "7" = "Pastures", "8" = "Grassland", "9" = "Transitional", "10" = "Coniferous forest", 
+                             "7" = "Pastures", "8" = "Grassland", 
+                             "9" = "Transitional", "10" = "Coniferous forest", 
                              "11" = "Mixed forest", "12" = "Broadleaf forest")) %>%
-  mutate(landCover2 = factor(landCover2, levels = c("Built area", "Saltwater related", "No vegetation", 
-                                                    "Freshwater related", "Other vegetation", "Agricultural land", 
-                                                    "Pastures", "Grassland", "Transitional", "Coniferous forest", 
-                                                    "Mixed forest", "Broadleaf forest")))
+  mutate(landCover2 = factor(landCover2, levels = c("Built area", 
+                                                    "Saltwater related", 
+                                                    "No vegetation", 
+                                                    "Freshwater related", 
+                                                    "Other vegetation", 
+                                                    "Agricultural land", 
+                                                    "Pastures", "Grassland", 
+                                                    "Transitional", 
+                                                    "Coniferous forest", 
+                                                    "Mixed forest", 
+                                                    "Broadleaf forest")))
   
 
 tcd_plot <- ggplot(PA_covars) + 
@@ -148,7 +163,8 @@ PO_data_sf <- PO_data %>%
   st_as_sf(coords = c("X", "Y"), crs = st_crs(ireland)) 
 
 PO_win <- as.owin(st_bbox(ireland))
-PO_ppp <- ppp(x = st_coordinates(PO_data_sf)[,1], y = st_coordinates(PO_data_sf)[,2], 
+PO_ppp <- ppp(x = st_coordinates(PO_data_sf)[,1], 
+              y = st_coordinates(PO_data_sf)[,2], 
               window = PO_win)
 PO_dens <- density(PO_ppp, sigma = bw.diggle, eps = c(500, 500))
 PO_raster <- raster(PO_dens)
@@ -158,14 +174,18 @@ PO_raster2 <- raster::mask(PO_raster, ireland)
 plot(PO_raster2)
 PO_raster2[PO_raster2 < 0.0000001] <- NA
 
-RL <- list(env_data$landCover, env_data$tree_cover_density, env_data$elevation, env_data$slope, env_data$human_footprint_index, PO_raster2)
+RL <- list(env_data$landCover, env_data$tree_cover_density, 
+           env_data$elevation, env_data$slope, env_data$human_footprint_index, 
+           PO_raster2)
 # devtools::source_url("https://github.com/VirginiaMorera/Useful-little-functions/blob/master/list_to_stack.R?raw=TRUE")
 
-new_stack <- list_to_stack(RL, new_res = raster::res(env_data), dest_crs = env_data@crs, turn_0_to_NA = T)
+new_stack <- list_to_stack(RL, new_res = raster::res(env_data), 
+                           dest_crs = env_data@crs, turn_0_to_NA = T)
 
 plot(new_stack)
 names(new_stack)[6] <- "Fallow_deer_per_cell"
 cor <-layerStats(new_stack, 'pearson', na.rm = T)
-ggcorrplot(cor$`pearson correlation coefficient`, method = "circle", show.diag = F, type = "upper", lab = T) 
+ggcorrplot(cor$`pearson correlation coefficient`, method = "circle", 
+           show.diag = F, type = "upper", lab = T) 
 ggsave("outputs/fallowDeerCorr.png")
 
