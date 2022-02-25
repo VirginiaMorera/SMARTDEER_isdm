f231ebf8449834bb1066c5ec3e73b2cb55bb7c8c
VirginiaMorera <morera.virginia@gmail.com>
2022-02-25 14:16:17 +0000

    silly changes

--- scripts/setups.R
+++ scripts/setups.R
@@ -1,6 +1,8 @@
 # install all necessary packages # 
 # devtools::install_github('PhilipMostert/inlabruSDMs', force = T)
-# install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
+# install.packages("INLA",repos=c(getOption("repos"),
+#                                 INLA="https://inla.r-inla-download.org/R/stable"), 
+#                  dep=TRUE)
 library(tidyverse)
 library(sf)
 library(sp)
@@ -18,7 +20,8 @@ library(viridis)
 library(INLA)
 library(ggpmisc)
 
-projKM <- "+proj=tmerc +lat_0=53.5 +lon_0=-8 +k=0.99982 +x_0=600000 +y_0=750000 +ellps=GRS80 +units=km +no_defs"
+projKM <- "+proj=tmerc +lat_0=53.5 +lon_0=-8 +k=0.99982 +x_0=600000 +y_0=750000
+           +ellps=GRS80 +units=km +no_defs"
 
 `%!in%` = Negate(`%in%`)
 
