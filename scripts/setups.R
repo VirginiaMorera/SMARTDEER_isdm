aa549348a71e8bcf0ac84cfe13c3e7ed8f024d03
VirginiaMorera <morera.virginia@gmail.com>
2022-03-04 10:21:52 +0000

    changes to get validation plots ready for publication

--- scripts/setups.R
+++ scripts/setups.R
@@ -20,6 +20,7 @@ library(viridis)
 library(INLA)
 library(ggpmisc)
 library(climateStability)
+library(ggrepel)
 
 projKM <- "+proj=tmerc +lat_0=53.5 +lon_0=-8 +k=0.99982 +x_0=600000 +y_0=750000
            +ellps=GRS80 +units=km +no_defs"
