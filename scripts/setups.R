6be14b0d0e43c583e5d2bb37a3737f9a8c836d8f
VirginiaMorera <morera.virginia@gmail.com>
2022-08-24 16:17:56 +0100

    add packages needed for correlations and validation

--- scripts/setups.R
+++ scripts/setups.R
@@ -21,6 +21,10 @@ library(INLA)
 library(ggpmisc)
 library(climateStability)
 library(ggrepel)
+library(ggthemes)
+library(beepr)
+library(rstatix)
+library(magrittr)
 
 
 projKM <- "+proj=tmerc +lat_0=53.5 +lon_0=-8 +k=0.99982 +x_0=600000 +y_0=750000
