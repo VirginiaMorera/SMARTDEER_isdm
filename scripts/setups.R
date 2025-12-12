d82f1f01b1c78f0acd59085b8a1a4df2475bda28
Virginia Morera-Pujol <morera.virginia@gmail.com>
2025-12-12 15:05:42 +0000

    add terra packages

--- scripts/setups.R
+++ scripts/setups.R
@@ -25,6 +25,9 @@ library(ggthemes)
 library(beepr)
 library(rstatix)
 library(magrittr)
+library(terra)
+library(tidyterra)
+library(patchwork)
 
 projKM <- "+proj=tmerc +lat_0=53.5 +lon_0=-8 +k=0.99982 +x_0=600000 +y_0=750000
            +ellps=GRS80 +units=km +no_defs"
